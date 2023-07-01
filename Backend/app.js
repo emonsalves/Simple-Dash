01e784a4db9cd2545ad395c1a66c7b4b059e02ff
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-01 10:21:09 -0400

    Feat : new package for dev utils

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
+const morganBody = require('morgan-body')
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+
 const app = express();
 
 // Middlewares
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
+morganBody(app);
 
 // Routes
 app.use(express.static(('public')))
