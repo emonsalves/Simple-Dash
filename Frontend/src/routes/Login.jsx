f0fccb654e971afcd64e18507006aba491fff11a
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-01 03:43:51 -0400

    Fix : forms and styles

--- Frontend/src/routes/Login.jsx
+++ Frontend/src/routes/Login.jsx
@@ -2,9 +2,10 @@ import DefaultLayout from "../layout/DefaultLayout";
 
 function Login() {
   return (
+    <>
     <DefaultLayout>
       <form className="form">
-        <h3>Log in</h3>
+        <h1>Log in</h1>
         <div>
           <label htmlFor="email">Username</label>
           <input type="text" id="text" />
@@ -16,6 +17,7 @@ function Login() {
         <button type="submit">Log in</button>
       </form>
     </DefaultLayout>
+    </>
   );
 }
 export default Login;
--- Frontend/src/routes/Signup.jsx
+++ Frontend/src/routes/Signup.jsx
@@ -1,6 +1,34 @@
+import DefaultLayout from "../layout/DefaultLayout";
 function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <>
+      <DefaultLayout>
+        <form className="form">
+          <h1>Signup</h1>
+          <div className="form-control">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="passwordConfirmation">Confirm Password</label>
+            <input
+              type="password"
+              id="passwordConfirmation"
+              name="passwordConfirmation"
+            />
+          </div>
+          <button type="submit">Signup</button>
+        </form>
+      </DefaultLayout>
+    </>
+  );
 }
-export default Signup
+export default Signup;
