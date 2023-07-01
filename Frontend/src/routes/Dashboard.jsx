d5136b8e97595d60a35cf88030ecfd54a2801d1d
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-01 09:35:12 -0400

    Feat : Logout

--- Frontend/src/routes/Dashboard.jsx
+++ Frontend/src/routes/Dashboard.jsx
@@ -1,6 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../auth/AuthProvider";
+
 function Dashboard() {
+  const {isAuthenticated, setIsAuthenticated} = useAuthContext();
+  const goTo = useNavigate();
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(" User logged out successfully");
+      setIsAuthenticated(!isAuthenticated);
+      goTo("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <h1>Dashboard</h1>
-  )
+    <form className="form" onSubmit={handleSubmit}>
+      <h1>Dashboard</h1>
+      <button type="submit">Log out</button>
+    </form>
+  );
 }
-export default Dashboard
+export default Dashboard;
