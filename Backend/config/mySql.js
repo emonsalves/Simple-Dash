3f0bd0ddc6560a49b887adfa35b039efda72c1e9
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-01 09:50:55 -0400

    Feat : mysql service and package

---
+++ Backend/config/mySql.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const credentials = {
+  host: process.env.MYSQL_DB_HOST,
+  user: process.env.MYSQL_DB_USER,
+  password: process.env.MYSQL_DB_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
+
+module.exports = { credentials }
