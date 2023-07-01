fa44f178fce768c3defd7e8a65739a7b9b4b1426
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-01 02:48:05 -0400

    Feat : router and basic protected route

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./routes/Login.jsx";
+import Signup from "./routes/Signup.jsx";
+import Dashboard from "./routes/Dashboard.jsx";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ Frontend/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,6 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+export default function ProtectedRoute() {
+    const [isAuth, setIsAuth] = useState(true);
+    return isAuth ? <Outlet /> : <Navigate to="/" />;
+}
