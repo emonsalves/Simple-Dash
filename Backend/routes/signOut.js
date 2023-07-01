a1d10307b7ed2edbdb528f9dac9d7c80759ee5eb
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-01 07:44:50 -0400

    Fix : signup ready

--- Backend/routes/signOut.js
+++ Backend/routes/signOut.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
+const { jsonResponse } = require('../lib/jsonResponse')
 
 const router = Router()
 
 router
   .get('/', (req, res) => {
     res.send('signOut')
-    })
+  })
 
 module.exports = router
--- Backend/routes/signUp.js
+++ Backend/routes/signUp.js
@@ -1,10 +1,21 @@
 const { Router } = require('express')
+const { jsonResponse } = require('../lib/jsonResponse')
 
 const router = Router()
 
 router
-  .get('/', (req, res) => {
-    res.send('signUp')
-    })
+  .post('/', (req, res) => {
+    const { userName, password, passwordConfirmation } = req.body
+
+    if (!!userName && !!password && !!passwordConfirmation) {
+      return res.status(400)
+        .json(jsonResponse(400, { message: 'Missing required fields' }))
+    }
+
+    //crear usuario en la data base 
+    res
+      .status(200)
+      .json(jsonResponse(200, { message: 'User created' }))
+  })
 
 module.exports = router
