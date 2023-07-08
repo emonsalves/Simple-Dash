2aab35f67fcd74f5042c011747857b0d0935e534
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-07 20:35:35 -0400

    Feat : Get All Users

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { jsonResponse } from "../lib/jsonResponse.js";
+import { User } from "../models/Proyect.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("User!");
+router.get("/", async (req, res) => {
+  const users = await User.findAll();
+  res.json(jsonResponse(200, { users, ok: true }));
 });
 
 export default router;
