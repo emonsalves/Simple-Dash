b57f1e50446c3b2e95bb8a9afeb860a5758e38da
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-09 17:10:47 -0400

    Fix : Better Reader MVC

--- Backend/routes/login.route.js
+++ Backend/routes/login.route.js
@@ -1,39 +1,7 @@
 import { Router } from "express";
-import { jsonResponse } from "../lib/jsonResponse.js";
-import { User } from "../models/Proyect.js";
-import { compare } from "../utils/bcypt.js";
-
+import { LoginUser } from "../controllers/login.controller.js";
 const router = Router();
 
-router.post("/", async (req, res) => {
-  const { userName, password } = req.body;
-
-  if (!userName || !password) {
-    return res
-      .status(400)
-      .json(jsonResponse(400, { message: "Missing required fields" }));
-  }
-
-  const user = await User.findOne({ where: { user_name: userName } });
-
-  if (!user) {
-    return res
-      .status(404)
-      .json(jsonResponse(404, { message: "User not found" }));
-  }
-
-  const validPassword = await compare({ text: password, hash: user.password });
-
-  if (!validPassword) {
-    return res
-      .status(400)
-      .json(jsonResponse(400, { message: "Invalid password or user" }));
-  }
-
-  const accessToken = "access-token";
-  const refreshToken = "refresh-token";
-
-  res.status(200).json(jsonResponse(200, { accessToken, refreshToken, user }));
-});
+router.post("/", LoginUser);
 
 export default router;
