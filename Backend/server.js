23fb4906a9aaaf76feb0ad130236f8fd7d2daca5
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-09 21:27:25 -0400

    Feat : server functions leave start

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,22 @@
 import app from "./app.js";
-import "./models/Proyect.js";
+import "./models/User.js";
+import { createRoles } from "./models/RoleGenerator.js";
 
 const port = process.env.PORT || 5000;
 
 import { jsonResponse } from "./lib/jsonResponse.js";
-import { sequelize } from "./database/users.js";
+import { sequelize } from "./database/ProyectZero.js";
 
 async function force() {
   try {
-    sequelize.sync({ force: true });
+    // sequelize.sync({ force: true });
     console.log("Tables created successfully");
+    // createRoles();
+    console.log("Default roles generated successfully");
   } catch (error) {
     jsonResponse(500, { error: error.message });
   }
-  
+
   try {
     app.listen(port);
     console.log(`Server on port ${port}`);
