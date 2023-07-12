8b2e1e2f02a5c5bc189b8c04ab1220d52ffc1e9a
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-12 15:18:58 -0400

    Delete : old files

--- Frontend/src/routes/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-import { useAuthContext } from "../context/AuthContext";
-
-function ProtectedRoute() {
-  const auth = useAuthContext();
-  return auth.isAuthenticated ? <Outlet /> : <Navigate to="/" />;
-}
-
-export default ProtectedRoute;
