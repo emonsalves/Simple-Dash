7b3b36d9aae0f9117453e429014e19b2e032666c
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-11 22:22:28 -0400

    Fix : comment

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,8 +1,6 @@
 import { User } from "./user.js";
 import { Role } from "./role.js";
 
-// Relación uno a muchos (un rol puede tener muchos usuarios)
-
 User.belongsTo(Role, {
   foreignKey: {
     name: "roleId",
