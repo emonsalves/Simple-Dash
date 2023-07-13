96111b14473fff01b3b2859739be9981e64466de
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-13 18:38:09 -0400

    Fix : userController better logic

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,5 @@
 import { jsonResponse } from "../lib/jsonResponse.js";
-import { User, Role } from "../models/index.js";
-import { bcryptUtil } from "../utils/bcypt.js";
+import { userService } from "../services/user.Service.js";
 
 const login = async (req, res) => {
   const { userName, password } = req.body;
@@ -11,72 +10,61 @@ const login = async (req, res) => {
       .json(jsonResponse(400, { message: "Missing required fields" }));
   }
 
-  const user = await User.findOne({
-    where: { user_name: userName },
-    include: { model: Role, attributes: ["name"] },
-  });
-
-  if (!user) {
-    return res
-      .status(404)
-      .json(jsonResponse(404, { message: "User not found" }));
+  try {
+    const result = await userService.login(userName, password);
+    res.status(result.status).json(jsonResponse(result.status, result.data));
+  } catch (error) {
+    res.status(500).json(jsonResponse(500, { message: error.message }));
   }
-
-  const validPassword = await bcryptUtil.compare({
-    text: password,
-    hash: user.password,
-  });
-
-  if (!validPassword) {
-    return res
-      .status(400)
-      .json(jsonResponse(400, { message: "Invalid password or user" }));
-  }
-
-  delete user.dataValues.password;
-
-  const accessToken = "access-token";
-  const refreshToken = "refresh-token";
-
-  res.status(200).json(jsonResponse(200, { accessToken, refreshToken, user }));
 };
 
 const getAll = async (req, res) => {
-  const users = await User.findAll();
-  res.json(jsonResponse(200, { users }));
+  try {
+    const result = await userService.getAllUsers();
+    res.status(result.status).json(jsonResponse(result.status, result.data));
+  } catch (error) {
+    res.status(500).json(jsonResponse(500, { message: error.message }));
+  }
 };
 
 const getOne = async (req, res) => {
   const { username } = req.params;
-  const user = await User.findOne({ where: { user_name: username } });
-  user
-    ? res.json(jsonResponse(200, { user, ok: true }))
-    : res.json(jsonResponse(404, { message: "User not found" }));
+
+  try {
+    const result = await userService.getUserByUsername(username);
+    res.status(result.status).json(jsonResponse(result.status, result.data));
+  } catch (error) {
+    res.status(500).json(jsonResponse(500, { message: error.message }));
+  }
 };
 
 const update = async (req, res) => {
   const { username } = req.params;
   const { name, last_name, phone, address } = req.body;
-  const updatedUser = await User.update(
-    {
+
+  try {
+    const result = await userService.updateUserByUsername(
+      username,
       name,
       last_name,
       phone,
-      address,
-    },
-    { where: { user_name: username } }
-  );
-  updatedUser
-    ? res.json(jsonResponse(200, { username }))
-    : res.json(jsonResponse(404, { message: "User not found" }));
+      address
+    );
+    res.status(result.status).json(jsonResponse(result.status, result.data));
+  } catch (error) {
+    res.status(500).json(jsonResponse(500, { message: error.message }));
+  }
 };
 
 const deleted = async (req, res) => {
   const { username } = req.params;
-  const deletedUser = await User.destroy({ where: { user_name: username } });
-  deletedUser
-    ? res.json(jsonResponse(200, { username }))
-    : res.json(jsonResponse(404, { message: "User not found" }));
+
+  try {
+    const result = await userService.deleteUserByUsername(username);
+    res.status(result.status).json(jsonResponse(result.status, result.data));
+  } catch (error) {
+    res.status(500).json(jsonResponse(500, { message: error.message }));
+  }
 };
 
 const register = async (req, res) => {
@@ -85,7 +73,7 @@ const register = async (req, res) => {
   if (password !== passwordConfirmation) {
     return res
       .status(400)
-      .json(jsonResponse(400, { message: "Passwords do not match" }));
+      .json(jsonResponse(400, { message: "Passwords or do not match" }));
   }
 
   if (!userName || !password || !passwordConfirmation) {
@@ -95,28 +83,8 @@ const register = async (req, res) => {
   }
 
   try {
-    const existingUserName = await User.findOne({
-      where: { user_name: userName },
-    });
-
-    if (existingUserName) {
-      return res
-        .status(400)
-        .json(jsonResponse(400, { message: "Username already exists" }));
-    }
-
-    const encryptedPassword = bcryptUtil.encrypt({ text: password });
-
-    await User.create({
-      user_name: userName,
-      password: encryptedPassword,
-      name: "Test",
-      last_name: "Test",
-      // email: "test",
-      phone: "Test",
-      address: "Test",
-    });
-    res.status(201).json(jsonResponse(201, { message: "User created" }));
+    const result = await userService.registerUser(userName, password);
+    res.status(result.status).json(jsonResponse(result.status, result.data));
   } catch (error) {
     res.status(500).json(jsonResponse(500, { message: error.message }));
   }
