b1070eb8b602f71fb791058bb4b4d60f383e5a68
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-13 16:05:36 -0400

    Update : chaanged to ES

--- Backend/config/mySql.js
+++ Backend/config/mySql.js
@@ -1,18 +1,18 @@
-require('dotenv').config()
+import dotenv from "dotenv";
 
 const credentials = {
-  host: process.env.MYSQL_DB_HOST,
-  user: process.env.MYSQL_DB_USER,
-  password: process.env.MYSQL_DB_PASSWORD,
-  database: process.env.MYSQL_DB_NAME,
+  host: dotenv.config().parsed.MYSQL_DB_HOST,
+  user: dotenv.config().parsed.MYSQL_DB_USER,
+  password: dotenv.config().parsed.MYSQL_DB_PASSWORD,
+  database: dotenv.config().parsed.MYSQL_DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10,
   idleTimeout: 60000,
   queueLimit: 0,
   ssl: {
-    rejectUnauthorized: false
-  }
-}
+    rejectUnauthorized: false,
+  },
+};
 
-module.exports = { credentials }
+export { credentials };
