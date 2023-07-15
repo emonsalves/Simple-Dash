23018e46783e0b07610406e923aca7244f805515
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-15 18:13:16 -0400

    Update : best read

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import AuthProvider from "./context/AuthContext";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
