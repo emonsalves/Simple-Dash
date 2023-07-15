d14af288a7ca4c5d44afb2017ca4c5a53404615a
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-15 00:45:31 -0400

    Fix : security

--- Backend/config/mail.config.js
+++ Backend/config/mail.config.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const mailConfig = {
-  host: "smtp-mail.outlook.com", // hostname
+  host:   process.env.MAIL_HOST, // hostname
   secureConnection: false, // TLS requires secureConnection to be false
-  port: 587, // port for secure SMTP
+  port: process.env.MAIL_PORT, // port for secure SMTP
   tls: {
     ciphers: "SSLv3",
   },
