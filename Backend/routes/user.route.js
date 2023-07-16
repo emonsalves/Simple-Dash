e12551915ecb82faee8546261ea140e9b629ddfe
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-16 01:19:02 -0400

    Feat : using middleware auth

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { userController } from "../controllers/user.controller.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 router.post("/register", userController.register);
-router.get("/all", userController.getAll);
+router.get("/all", verifyToken, userController.getAll);
 router.get("/:username", userController.getOne);
 router.put("/:username", userController.update);
 router.delete("/:username", userController.deleted);
