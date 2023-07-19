34ca3b26857ac3b45ebeaeae5a535ef5b34f69a5
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-18 20:03:08 -0400

    Feat : security using env

--- Backend/database/ProyectZero.js
+++ Backend/database/ProyectZero.js
@@ -1,14 +1,22 @@
 import Sequelize from "sequelize";
+import dotenv from "dotenv";
 
-export const sequelize = new Sequelize("projectsdb", "root", "12qwaszx", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    require: 30000,
-    idle: 10000,
-  },
-  logging: false,
-  timezone: "-04:00", // Ajusta el horario a Chile
-});
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.SEQUELIZE_DATABASE,
+  process.env.SEQUELIZE_USER,
+  process.env.SEQUELIZE_PASSWORD,
+  {
+    host: process.env.SEQUELIZE_HOST,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      require: 30000,
+      idle: 10000,
+    },
+    logging: false,
+    timezone: "-04:00", // Ajusta el horario a Chile
+  }
+);
