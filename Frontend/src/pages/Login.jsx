c47161820b9b0cc66d699cf5e0befa3cbb03370b
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-20 22:22:31 -0400

    Fix : better read

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Navigate, useNavigate,  } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
 
 import { login } from "../api/auth";
@@ -8,7 +8,8 @@ function Login() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [errorResponse, setErrorResponse] = useState("");
-  const { isAuthenticated, setIsAuthenticated, setUser } = useAuthContext();
+  const { isAuthenticated, setIsAuthenticated, setUser, setTokens } =
+    useAuthContext();
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
@@ -24,11 +25,22 @@ function Login() {
   const handleResponse = (response) => {
     const { statusCode, body } = response;
     if (statusCode === 200) {
-      localStorage.setItem("tokens", JSON.stringify(body));
+      localStorage.setItem(
+        "tokens",
+        JSON.stringify({
+          accessToken: body.accessToken,
+          refreshToken: body.refreshToken,
+        })
+      );
+      localStorage.setItem("user", JSON.stringify(body.user));
       console.log("User logged in successfully");
       setErrorResponse("");
-      setIsAuthenticated(!isAuthenticated);
-      setUser(body);
+      setIsAuthenticated(true);
+      setTokens({
+        accessToken: body.accessToken,
+        refreshToken: body.refreshToken,
+      });
+      setUser(body.user);
       navigate("/");
     } else {
       console.log("message: ", body.message);
@@ -42,34 +54,34 @@ function Login() {
   }
 
   return (
-      <form className="form" onSubmit={handleSubmit}>
-        <h1>Log in</h1>
-        {!!errorResponse && <div className="errorMessage">{errorResponse}</div>}
-        <div>
-          <label htmlFor="username">User</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={userName}
-            onChange={(event) => setUserName(event.target.value)}
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Log in</button>
-        <button type="button" onClick={() => navigate("/auth/forgot-password")}>
-          Forgot Password
-        </button>
-      </form>
+    <form className="form" onSubmit={handleSubmit}>
+      <h1>Log in</h1>
+      {!!errorResponse && <div className="errorMessage">{errorResponse}</div>}
+      <div>
+        <label htmlFor="username">User</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Log in</button>
+      <button type="button" onClick={() => navigate("/auth/forgot-password")}>
+        Forgot Password
+      </button>
+    </form>
   );
 }
 
