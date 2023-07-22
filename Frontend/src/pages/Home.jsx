f85d685d85b23975dcff41858ac169b5fd832842
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-21 23:37:28 -0400

    Feat : best use

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/auth">Go to Login</Link>
+    </div>
+  );
 }
 export { Home };
