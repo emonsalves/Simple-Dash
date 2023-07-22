397a7397ea9fb4cd252365dedf3fac9205469119
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-22 19:40:47 -0400

    Fix : best Style and logic

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,11 @@
-import { searchUsers } from "../api/user";
+import { useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { searchUsers } from "../api/user";
+import { Button } from "../components/Button/ButtonMagic";
 
 function Dashboard() {
-  const { user, tokens, logOut } = useAuthContext();
-  const goTo = useNavigate();
-
-
-  const handleLogout = async (e) => {
-    e.preventDefault();
-    logOut();
-    goTo("/auth");
-  };
+  const { user, tokens } = useAuthContext();
+  const [searchResult, setSearchResult] = useState("");
 
   const onClick = async (e) => {
     e.preventDefault();
@@ -20,22 +14,31 @@ function Dashboard() {
         userName: user.user_name,
         token: tokens.accessToken,
       });
-      return console.log(response.body.message || response.body);
+      setSearchResult(response.body.users);
     } catch (error) {
       console.log(error);
     }
   };
 
-
   return (
     <>
       {user && (
-        <form className="form">
-          <h1>Dashboard</h1>
-          <h2>Welcome {user.user_name}</h2>
-          <h2>Role: {user.Role.name}</h2>
-          <button onClick={onClick}>Search User</button>
-          <button onClick={handleLogout}>Logout</button>
+        <form className="form flex flex-col justify-between md:w-2/3 lg:w-1/2 xl:w-1/3 p-2 mx-auto">
+          <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+          <h2 className="mb-2">Welcome {user.user_name}</h2>
+          <h2 className="mb-4">Role: {user.Role.name}</h2>
+          <Button
+            text="Search Users"
+            action={onClick}
+            tailwind="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
+          />
+          {Array.isArray(searchResult) &&
+            searchResult.map((user) => (
+              <h2 key={user.id} className="mb-4">
+                {user.user_name}
+              </h2>
+            ))}
+
         </form>
       )}
     </>
