beb39f2cf6111ac557e1c4abe2ad0c714709cf1f
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-21 23:21:29 -0400

    Fix : best read & style

--- Frontend/src/pages/NotFound.jsx
+++ Frontend/src/pages/NotFound.jsx
@@ -1,6 +1,16 @@
+import { Link, useRouteError } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <>
+      <h1>404</h1>
+      <p>{error.statusText || error.message}</p>
+      <Link to="/">Go to Home</Link>
+    </>
+  );
 };
 
 export { NotFound };
