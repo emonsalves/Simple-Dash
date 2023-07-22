f479eb165d657b02e6e3ffd4e5c5701495e026c5
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-22 15:01:12 -0400

    Fix : Best read and Style

--- Frontend/src/components/Navbar/NavPublic.jsx
+++ Frontend/src/components/Navbar/NavPublic.jsx
@@ -5,12 +5,10 @@ const NavPublic = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const goTo = useNavigate();
 
-  const goToLogin = () => {
-    goTo("/auth");
-  };
-  const goToRegister = () => {
-    goTo("/auth/register");
-  };
+  const menuItems = [
+    { text: "Login", onClick: () => goTo("/auth") },
+    { text: "Register", onClick: () => goTo("/auth/register") },
+  ];
 
   const handleMobileMenuToggle = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -20,15 +18,20 @@ const NavPublic = () => {
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <span className="text-white text-xl">Mi Sitio</span>
+          <span className="text-white text-xl hover:text-white cursor-pointer">
+            Mi Sitio
+          </span>
         </div>
         <div className="hidden md:flex space-x-4">
-          <a onClick={goToLogin} className="text-gray-300 hover:text-white">
-            Login
-          </a>
-          <a onClick={goToRegister} className="text-gray-300 hover:text-white">
-            Register
-          </a>
+          {menuItems.map((item, index) => (
+            <a
+              key={index}
+              onClick={item.onClick}
+              className="text-gray-300 hover:text-white cursor-pointer"
+            >
+              {item.text}
+            </a>
+          ))}
         </div>
         <div className="md:hidden flex items-center">
           <button
@@ -41,12 +44,15 @@ const NavPublic = () => {
       </div>
       <div className={`${isMobileMenuOpen ? "" : "hidden"} md:hidden`}>
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 flex flex-col">
-          <a onClick={goToLogin} className="text-gray-300 hover:text-white">
-            Login
-          </a>
-          <a onClick={goToRegister} className="text-gray-300 hover:text-white">
-            Register
-          </a>
+          {menuItems.map((item, index) => (
+            <a
+              key={index}
+              onClick={item.onClick}
+              className="text-gray-300 hover:text-white"
+            >
+              {item.text}
+            </a>
+          ))}
         </div>
       </div>
     </nav>
