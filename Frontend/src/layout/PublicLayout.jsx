8011510eac016f994ceb355120673ab5c8cc498e
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-22 19:40:30 -0400

    Fix : Best Style

--- Frontend/src/layout/PublicLayout.jsx
+++ Frontend/src/layout/PublicLayout.jsx
@@ -1,23 +1,13 @@
-import { useEffect } from "react";
-import { Outlet, Navigate } from "react-router-dom";
-import { useAuthContext } from "../context/AuthContext";
-import { Header } from "../components/Header";
+import { Outlet } from "react-router-dom";
+
 import { NavPublic } from "../components/Navbar/NavPublic";
 
 function PublicLayout() {
-  const { user } = useAuthContext();
-
-  // useEffect(() => {
-  //   if (user) {
-  //     <Navigate to="/" />;
-  //   }
-  // }, [user]);
 
   return (
     <div className="flex flex-col w-full bg-red-500">
-      {/* <Header /> */}
       <NavPublic />
-      <div className="flex flex-col w-full h-screen overflow-y-auto bg-green-500">
+      <div className="flex flex-col w-full h-screen justify-center items-center overflow-y-auto bg-green-500">
         <Outlet />
       </div>
     </div>
