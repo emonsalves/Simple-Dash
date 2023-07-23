d9c35fcbc9e88765efa16deb14b5d7c7e7bdc10a
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-23 00:39:37 -0400

    Added : new navbar simply

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,5 +1,48 @@
+import  { useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prevState) => !prevState);
+  };
+
+  return (
+    <nav className="bg-gray-800 shadow">
+      <div className="px-8 mx-auto">
+        <div className="flex items-center justify-end h-16">
+          <div className="relative">
+            <button
+              type="button"
+              className="flex items-center justify-center w-10 h-10 rounded-full text-gray-50 hover:bg-gray-500 focus:outline-none"
+              onClick={toggleDropdown}
+              id="options-menu"
+            >
+              <FaUserCircle className="w-6 h-6" />
+            </button>
+            {isDropdownOpen && (
+              <div className="absolute right-0 mt-2 w-56 bg-gray-800 rounded-md shadow-lg ring-1 ring-black ring-opacity-5">
+                <div
+                  className="py-1"
+                  role="menu"
+                  aria-labelledby="options-menu"
+                >
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-md text-gray-100 hover:bg-gray-600"
+                    role="menuitem"
+                  >
+                    Logout
+                  </a>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export { Header };
