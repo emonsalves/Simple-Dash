b24634d5a7f90585780549f934f3d10721c347d1
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-23 22:01:58 -0400

    Fix : best style

--- Frontend/src/layout/PublicLayout.jsx
+++ Frontend/src/layout/PublicLayout.jsx
@@ -3,11 +3,10 @@ import { Outlet } from "react-router-dom";
 import { NavPublic } from "../components/Navbar/NavPublic";
 
 function PublicLayout() {
-
   return (
     <div className="flex flex-col w-full transition-all duration-300 ">
       <NavPublic />
-      <div className="flex flex-col w-full h-screen justify-center items-center overflow-y-auto ">
+      <div className="flex flex-col w-full h-screen justify-center items-center overflow-y-auto">
         <Outlet />
       </div>
     </div>
