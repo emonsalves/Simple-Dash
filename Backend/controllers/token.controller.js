346a4d851d0d968c4f0f523d57ebd2a7a38a3651
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-26 23:52:26 -0400

    Fix : best read

--- Backend/controllers/token.controller.js
+++ Backend/controllers/token.controller.js
@@ -2,9 +2,10 @@ import { jsonResponse } from "../lib/jsonResponse.js";
 import { tokenService } from "../services/token.service.js";
 
 const refreshToken = async (req, res) => {
-  const { user_name } = req.body;
+  const { userName } = req.body;
+
   try {
-    const result = await tokenService.getNewToken({ user_name });
+    const result = await tokenService.getNewToken({ userName });
     res.status(result.status).json(jsonResponse(result.status, result.data));
   } catch (error) {
     res.json(jsonResponse(res.status, { message: error.message }));
