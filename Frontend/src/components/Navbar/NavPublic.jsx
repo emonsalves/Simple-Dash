99bf2b01938e41f995fcdbf89844d4ec68bc06c5
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-28 10:41:35 -0400

    fix : style best

--- Frontend/src/components/Navbar/NavPublic.jsx
+++ Frontend/src/components/Navbar/NavPublic.jsx
@@ -16,17 +16,17 @@ const NavPublic = () => {
   };
 
   return (
-    <nav className="bg-gray-800 p-4 fixed w-full z-10 top-0 shadow">
+    <nav className="bg-gray-800 p-3 fixed w-full z-10 top-0 shadow">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center hover:text-white cursor-pointer">
           <span className="text-white text-xl ">My Site</span>
         </div>
-        <div className="hidden md:flex space-x-4  ">
+        <div className="hidden md:flex">
           {menuItems.map((item, index) => (
             <a
               key={index}
               onClick={() => goTo(item.path)}
-              className={`hover:text-white hover:bg-gray-700 flex items-center px-3 py-2 rounded-md text-sm font-medium ${
+              className={`hover:text-white hover:bg-gray-700 flex items-center px-4 py-2 rounded-md text-sm font-medium ${
                 location.pathname === item.path ? "text-white" : "text-gray-300"
               } cursor-pointer`}
             >
