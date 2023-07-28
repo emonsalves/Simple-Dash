559296d3595b789530bf3d6db1c7460f0d84cfef
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-27 23:13:52 -0400

    fix : best read

--- Frontend/src/layout/AuthLayout.jsx
+++ Frontend/src/layout/AuthLayout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
 import { Sidebar } from "../components/SideBar";
+import { NavAuth } from "../components/Navbar/NavAuth";
 
 function AuthLayout() {
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex flex-col w-full h-14">
-        <Header />
+        <NavAuth />
         <div className="flex flex-col w-full">
           <Outlet />
         </div>
