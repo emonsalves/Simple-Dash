c3275262a88fa249172285cb8e46f13ebb2e5412
Emonsalves <ingeniero.monsalves@gmail.com>
2023-07-30 19:58:22 -0400

    fix : html

--- Frontend/src/hooks/useSweetAlert.js
+++ Frontend/src/hooks/useSweetAlert.js
@@ -1,12 +1,13 @@
 import Swal from "sweetalert2";
 
 const useSweetAlert = () => {
-  const showAlert = ({ title, text, icon, timer }) => {
+  const showAlert = ({ title, text, icon, timer, html }) => {
     Swal.fire({
       title,
       text,
       icon,
       timer,
+      html,
     });
   };
 
